fbf7768b2957bdbab064af45a51e4f72acb2447f
Asmod4n <Asmod4n@users.noreply.github.com>
2021-06-13 11:48:12 +0200

    Update to latest argon2

--- build_config.rb
+++ build_config.rb
@@ -1,6 +1,9 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
+  conf.enable_sanitizer "address,undefined,leak"
+  conf.cc.flags << '-fno-omit-frame-pointer' << '-g' << '-ggdb'
   enable_debug
+  conf.enable_debug
   conf.enable_test
   conf.gembox 'full-core'
   conf.gem File.expand_path(File.dirname(__FILE__))
